cec3e2474e7a8f9ddbec76d76f97e58463742f84
Cy Howard <cyhoward@tesla.com>
2023-01-25 09:06:43 -0800

    Updating Custom User Model

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ("username", "is_staff", "is_manager", "is_admin", "is_active",)
+    list_filter = ("username", "is_staff", "is_manager", "is_admin", "is_active",)
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_manager", "is_admin", "is_active", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "username", "email", "password1", "password2", "is_staff", "is_manager", "is_admin",
+                "is_active", "groups", "user_permissions"
+            )}
+        ),
+    )
+    search_fields = ("username",)
+    ordering = ("username",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
