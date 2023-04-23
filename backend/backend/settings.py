62f09f9be22c807319671607166e1af459b11e87
clhVEGAS <58777995+clhVEGAS@users.noreply.github.com>
2023-04-23 16:55:51 -0700

    Minor changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,12 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: Keep the secret key used in production secret!
 SECRET_KEY = 'e0k#6sehn@c%4op)33i47%cq_^s49v-lvn3s^gqo&9%6+j(a&('
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: Don't run with debug turned on in production!
 DEBUG = True
 
+# SECURITY WARNING: This needs to be modified to match production server hosts only.
 ALLOWED_HOSTS = ['*']
 
 
